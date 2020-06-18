eacc918eca97401dad338113a69ffde23ac931aa
Kunal Kumar Shrivastava <31282058+Kunalkshrivastava@users.noreply.github.com>
2020-06-19 00:03:28 +0530

    Delete welcome-to-java.java

--- HackerRank/welcome-to-java.java
+++
@@ -1,9 +0,0 @@
-//Welcome to Java!
-
-public class Solution {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
-        System.out.println("Hello, World.\nHello, Java.");
-    }
-}
