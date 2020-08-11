6fec42174b3c6741e09420d0415563a8cf82b54e
Kunal Kumar Shrivastava <31282058+Kunalkshrivastava@users.noreply.github.com>
2020-08-11 17:17:21 +0530

    Update Find Majority Elements.cpp

--- Coding Blocks/Find Majority Elements.cpp
+++ Coding Blocks/Find Majority Elements.cpp
@@ -1,7 +1,50 @@
-//Find all the elements which appear more than floor(N/3) times in the given array
 #include <iostream>
 #include <vector>
 using namespace std;
+void majorityElement(vector<int>& nums) 
+    {
+        int sz = nums.size();
+        int num1 = -1, num2 = -1, count1 = 0, count2 = 0, i;
+        for (i = 0; i < sz; i++)
+        {
+            if (nums[i] == num1)
+                count1++;
+            else if (nums[i] == num2)
+                count2++;
+            else if (count1 == 0)
+            {
+                num1 = nums[i];
+                count1 = 1;
+            }    
+            else if (count2 == 0)
+            {
+                num2 = nums[i];
+                count2 = 1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> ans;
+        count1 = count2 = 0;
+        for (i = 0; i < sz; i++)
+        {
+            if (nums[i] == num1)
+                count1++;
+            else if (nums[i] == num2)
+                count2++;
+        }
+        if(count1 < sz/3 && count2 < sz/3)
+            cout<<"No Majority Elements";
+        else{
+        if (count1 > sz/3)
+            cout<<num1<<" ";
+        if (count2 > sz/3)
+            cout<<num2;
+        }
+    }
 int main() {
     int n,x,element1(0),element2(0),e1count(0),e2count(0);
     vector<int> a;
@@ -11,56 +54,5 @@ int main() {
         cin>>x;
         a.push_back(x);
     }
-    element1 = a[0];
-    e1count = 1;
-    for(int i =0; i<n; i++)
-    {
-        if(a[i]== element1)
-        {
-            e1count = e1count + 1;
-        }
-        else if(a[i]== element2)
-        {
-            e2count = e2count + 1;
-        }
-        else if(e1count == 0)
-        {
-            element1 = a[i];
-            e1count = 1;
-        }
-        else if(e2count == 0)
-        {
-            element2 = a[i];
-            e2count = 1;
-        }
-        else
-        {
-            e1count --;
-            e2count --;
-        }
-    }
-    e1count = 0;
-    e2count = 0;
-    for(auto i:a)
-    {
-        if(i== element1)
-            e1count = e1count + 1;
-        if(i== element2)
-            e2count = e2count + 1;
-    }
-        if(e1count < n/3 && e2count < n/3)
-            cout<<"No Majority Elements";
-        else if(e2count > n/3 && e1count > n/3)
-        {
-            if(element1>element2)
-                cout<<element1<<" "<<element2;
-            else
-                cout<<element2<<" "<<element1;
-        }
-        else if(e1count > n/3)
-            cout<<element1;
-        else if(e2count > n/3)
-            cout<<element2;
-
-       
+    majorityElement(a);
 }
